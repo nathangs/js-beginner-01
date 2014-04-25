56093a95d6b4511a9647e0c38da978ab1709e9a9
thefivetoes <justin.smith@thundertech.com>
2014-04-25 13:10:41 -0400

    better documentation

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+/*  open index.html in your web browser
+    refresh your browser when you make 
+    an edit to this file...*/
+
 (function(){
-  //call custom functions here...
+  /*  this function gets called on page load
+      for example, uncomment the following line:/*
+  
+  //alert('hello'); 
+  
+  /*call custom functions here...*/
   
 }());
 
-//create new functions below...
+/*create new functions below this line...*/
